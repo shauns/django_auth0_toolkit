941ca28ffe822b2b805d9f30a79f28810ee899a3
Shaun Stanworth <shaun.stanworth@googlemail.com>
2016-11-08 14:33:39 +0000

    Fix bugs with SSO decorator

--- django_auth0_toolkit/sso.py
+++ django_auth0_toolkit/sso.py
@@ -1,4 +1,7 @@
 # -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+import logging
 from functools import wraps
 
 import requests
@@ -8,6 +11,9 @@ from django.http.response import HttpResponseRedirect
 from django.shortcuts import resolve_url
 from django.utils.decorators import available_attrs
 from django.utils.six.moves.urllib.parse import urlparse
+
+
+logger = logging.getLogger(__name__)
 
 
 class Auth0(object):
@@ -57,6 +63,7 @@ def sso_fallback(
             auth_url,
             params=authorize_params,
         ).prepare()
+        logger.debug('Sending user straight to %s', prepared_request.url)
         return HttpResponseRedirect(prepared_request.url)
 
     # hit auth0's authorize endpoint -- we'll get a redirect either
@@ -67,6 +74,7 @@ def sso_fallback(
     # now presumably res has status 302
     if res.status_code != 302:
         # Yikes!
+        logger.error("Unable to handle non-redirect response")
         raise NotImplementedError()
 
     # intercept auth0's redirection -- if the user isn't authed we
@@ -76,12 +84,17 @@ def sso_fallback(
     if auth0_redirect_to.startswith(login_callback_url):
         # auth0 wants to go to the callback URL => the user is authed
         # let auth0's response propagate
+        logger.debug(
+            'Redirecting to %s as user already authenticated',
+            auth0_redirect_to
+        )
         return HttpResponseRedirect(auth0_redirect_to)
 
     else:
         # auth0 wants to go elsewhere => the user isn't authed
         # go to our own hosted login page -- as the normal
         # login_required decorator would.
+        logger.debug('Redirecting to built-in login as user not authenticated')
         return redirect_login_required_to_login(
             request, login_url, redirect_field_name
         )
@@ -106,6 +119,8 @@ def user_passes_test_with_sso(
         def _wrapped_view(request, *args, **kwargs):
             if test_func(request.user):
                 return view_func(request, *args, **kwargs)
+
+            logger.debug('SSO fallback as user not authenticated')
 
             return sso_fallback(
                 request,
@@ -156,7 +171,7 @@ def login_required_with_sso(
     to auth0's SSO authorize endpoint, and then to the log-in page if necessary
     """
     actual_decorator = user_passes_test_with_sso(
-        lambda u: u.is_authenticated,
+        lambda u: u.is_authenticated(),
         auth0_login_callback_view_name,
         auth0_authorization_url,
         login_url=login_url,
